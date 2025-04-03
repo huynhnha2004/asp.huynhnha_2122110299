892160b30e2b5c2f79ec48c5af0e3537818e9b39
huynhnha2004 <nhahuynh260606@gmail.com>
2025-04-03 13:20:29 +0700

    1

---
+++ ASP_HuynhNha_2122110299/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ASP_HuynhNha_2122110299.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_HuynhNha_2122110299.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ASP_HuynhNha_2122110299/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP_HuynhNha_2122110299.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
